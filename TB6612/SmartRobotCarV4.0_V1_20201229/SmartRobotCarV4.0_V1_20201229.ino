b0e795d27d153036ef0084d05794f74d1ce8fff5
jfaber <jfaber@stud.hs-heilbronn.de>
2021-11-23 14:24:51 +0100

    Enhanced State machine with callback functions

--- TB6612/SmartRobotCarV4.0_V1_20201229/SmartRobotCarV4.0_V1_20201229.ino
+++ TB6612/SmartRobotCarV4.0_V1_20201229/SmartRobotCarV4.0_V1_20201229.ino
@@ -2,79 +2,119 @@
 #include <arduino.h>
 #include <arduino-timer.h>
 #include <TimerThree.h>
+#include <FastLED.h>	
 
 enum RobiStates{
     Idle,
     Alerted,
-    Hunting
+    Hunting,
+    Init
 };
 
-int pirPin = 2;
-int pirStat = 0;
+const int pirPin = 2;
+const int alertedTimeThreshold = 120;
+const int PIN_RBGLED = 4;
+const int NUM_LEDS = 1;
 
 RobiStates currentState;
 int timerCounter = 0;
+int timeInAlerted = 0;
+int timerThreshold;
+
+
+CRGB leds[NUM_LEDS]; 
+
+typedef void (*voidFunction) (void); 
+voidFunction callbackStateFunction;
 
 
 void setup(){
-    init_Robi();
-    Timer1.init(1000000);
-    Timer1.attachInterrupt(timerISR);
-    pinMode(LED_BUILTIN, OUTPUT);
-    noInterrupts();
-    TCCR0A = 0;
-    TCCE0B = 0;
-    TCNT0 = 0;
-
-    OCR0A = 255;
-    TCCR0B |= (1 << WGM02)
-    TCCR0B |= (1 << CS02) | (1 << CS00)
-    TIMSK0 |= (1 << OCIE0A);
-    interrupts();
+    initRobi();
     
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    Timer3.initialize(1000000);
+    Timer3.attachInterrupt(timerISR);
+    FastLED.addLeds<NEOPIXEL, PIN_RBGLED>(leds, NUM_LEDS);
+    FastLED.setBrightness(200);
 }
 
 void loop(){
-    switch (currentState)
-    {
-    case Idle:
-        /* code */
-        digitalWrite(LED_BUILTIN, LOW);
-        if (pirStat == HIGH){
-            currentState = Alerted;
-        }
-        break;
-    case Alerted:
-        /* code */
-        digitalWrite(LED_BUILTIN, HIGH);
-        if (pirStat == LOW){
-            currentState = Idle;
-        }
-        break;
-    case Hunting:
-        /* code */
-        break;
-    default:
-        break;
+
+    
+}
+
+void initRobi(){
+    setRobiState(Init);
+    pinMode(pirPin, INPUT);
+    Serial.begin(9600);
+    
+}
+
+void setRobiState(RobiStates newState){
+    if (currentState != newState){
+        switch (newState)
+            {
+            case Init:
+                leds[0] = CRGB::Blue;
+                FastLED.show();
+                timerThreshold = 120;
+                callbackStateFunction = []() -> void {
+                    attachInterrupt(digitalPinToInterrupt(pirPin), pirISR, CHANGE);
+                    setRobiState(Idle);
+                };
+                break;
+            case Idle:
+                digitalWrite(LED_BUILTIN, HIGH);
+                leds[0] = CRGB::Green;
+                FastLED.show();
+                timerThreshold = 300;
+                callbackStateFunction = []() -> void{
+                    //move around random
+                };
+                break;
+            case Alerted:
+                timerThreshold = 60;
+                timeInAlerted = 0;
+                callbackStateFunction = []() -> void{
+                    // move around faster
+                    timeInAlerted = timeInAlerted + timerThreshold;
+                    if (timeInAlerted > alertedTimeThreshold){
+                        setRobiState(Idle);
+                    }
+                };
+                
+                break;
+            case Hunting:
+                // catch & follow the sound
+                callbackStateFunction = []() -> void{
+                    
+                };
+                timerThreshold = 1;
+                break;
+            default:
+                break;
+            }
+        currentState = newState;
     }
 }
 
-void init_Robi(){
-    currentState = Idle;
-    pinMode(pirPin, INPUT);
-    Serial.begin(9600);
-    attachInterrupt(digitalPinToInterrupt(pirPin), pirISR, CHANGE);
-}
-
 void pirISR(){
-    pirStat = digitalRead(pirPin);
+    int pirStat = digitalRead(pirPin);
+    if ((currentState == Idle) && (pirStat == HIGH)){
+        setRobiState(Alerted);
+    }else if ((currentState == Alerted) && (pirStat == HIGH)){
+        timeInAlerted = 0;
+    }
+    
 }
 
 void timerISR(){
     timerCounter = timerCounter + 1;
+    if (timerCounter > timerThreshold){
+        timerCounter = 0;
+        //digitalWrite(LED_BUILTIN, HIGH);
+        callbackStateFunction();
+    }
 }
 
-ISR(TIMER0_COMPA_vect){
-    
-    timerCounter = timerCounter + 1;
-}
