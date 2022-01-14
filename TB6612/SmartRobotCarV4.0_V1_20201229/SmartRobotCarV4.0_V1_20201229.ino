e4d388efdd5ac38c0b838b0ab12dbe0d5a72e79b
jfaber <jfaber@stud.hs-heilbronn.de>
2022-01-14 21:05:33 +0100

    It works

--- TB6612/SmartRobotCarV4.0_V1_20201229/SmartRobotCarV4.0_V1_20201229.ino
+++ TB6612/SmartRobotCarV4.0_V1_20201229/SmartRobotCarV4.0_V1_20201229.ino
@@ -4,7 +4,19 @@
 #include <arduino.h>
 #include <TimerFreeTone.h>
 #include <FastLED.h>
+#include <stdlib.h>
 
+
+const int pins[] = {A8, A9, A10, A11};
+const int dir[] = {45, 45, 135, 135};
+const int leftMics[] = {A8, A11};
+//float calib[] = {1.0f,1.43f,1.0f,2.22f};
+float calib[] = {1,1,1,1};
+const int pin_size = 4;
+const int threshold = 200;
+
+bool nd = false;
+int cnt[pin_size][2];
 
 enum RobiStates{
     Idle,
@@ -48,6 +60,8 @@ int timerTurnThreshold = 2000;
 int testTimeInIdle = 0;
 int testCounterHunting = 0;
 
+int micCounter = 1;
+
 //long buzzerFrequence = 1200;
 //long buzzerDuration = 100;
 int melody[] = { 150, 200, 250, 0, 150, 200, 250};
@@ -58,12 +72,13 @@ int durationFound[] = { 500, 500, 500, 500, 500, 500, 500};
 //initilize car speed for initial mode
 uint8_t speed = 100;
 // initialize meters to drive formard in cm --> not used in current version 
-int forwardDistance = 100; 
+int forwardDistance = 40; 
 int turnAngle = 90; // 90 degree
 
 volatile bool enabledDrive = false;
 volatile bool enabledTurn = false;
 volatile bool enabledHunting = false;
+volatile bool enabledMicArray = true;
 
 CRGB leds[NUM_LEDS]; 
 
@@ -84,15 +99,24 @@ void setup(){
     FastLED.setBrightness(200);
     initRobi();
 
+    for(int i = 0; i < pin_size; i++){
+        pinMode(pins[i], INPUT);
+    }
+
 }
 
 void loop(){
+    Serial.print("Can drive: ");
+    Serial.println(enabledDrive);
     if (stateTimer.hasTimedOut()){
       digitalWrite(LED_BUILTIN, HIGH);
+      enabledMicArray = false;
       callbackStateFunction();
       stateTimer.reset();
     }
     if (driveTimer.hasTimedOut()){
+        Serial.println("Timeout");
+        
       if (currentState == Idle){
         leds[0] = CRGB::Green;
         FastLED.show();
@@ -107,16 +131,15 @@ void loop(){
       driveTimer.setTimeOutTime(driverTimeOutTime);
       driveTimer.reset();
       stateTimer.reset();
-      //Serial.println("Driver timed out");
+      Serial.println("Set driver timer timeout based on statetimer");
+      delay(100);
+      enabledMicArray = true;
     }
-
-    
-    
-    //Serial.println(driveTimer.getElapsedTime());
     
     if (enabledDrive == true){
         callbackDriveStateFunction();
     }else{
+        
         if (currentDrivingState == Turning){
             setDrivingState(Forward);
         }else if(currentDrivingState == Forward){
@@ -135,6 +158,10 @@ void loop(){
         
       Application_FunctionSet.ApplicationFunctionSet_StopRobi();   
     }
+
+    if (enabledMicArray){
+        micDetect();
+    }
     
 }
 
@@ -146,16 +173,169 @@ void initRobi(){
     
 }
 
+void micDetect(){
+  int raw_values[pin_size];
+  int values[pin_size];
+  for(int i = 0; i < pin_size; i++){
+      raw_values[i] = (float)(analogRead(pins[i])-512)*calib[i];
+  }
+  for(int i = 0; i < pin_size; i++){
+    values[i] = abs((values[i] + raw_values[i]) / 2);
+  }
+  
+  bool new_noise = false;
+  for(int i = 0; (i < pin_size); i++){
+    if(values[i] >= threshold){
+      new_noise = true;
+    }
+  }
+
+  
+  if(new_noise && nd){
+    //Continued noise
+    Serial.println("Noise continued");
+    int max_pin = getMaxPin(values);
+    increaseMaxPinCount(max_pin);
+    printValues(values);
+  }
+  else if(nd && !new_noise){
+    //noise ended
+    Serial.println("Noise ended");
+    nd = false; 
+    int sortedPins[pin_size];
+    for(int i = 0; i < pin_size; i++){
+      sortedPins[i] = i;
+    }
+    //sort
+    for(int i = 0; i < pin_size; i++){
+      for(int j = i; j < pin_size; j++){
+        if(cnt[i][1] < cnt[j][1]){
+          int temp = sortedPins[i];
+          sortedPins[i] = sortedPins[j];
+          sortedPins[j] = temp;
+        }
+      }
+    }
+
+    for(int i = 0; i < pin_size; i++){
+      Serial.print(sortedPins[i]);
+    }
+    Serial.println("");
+
+    //Direction by pin with most counts
+    int first_pin = sortedPins[0];
+    int noise_direction = dir[first_pin];
+
+    //Addition by pin with second most counts
+    int second_pin = sortedPins[1];
+    if(((first_pin - second_pin) % 2) == 1){
+      // Pins are next to each other
+      if(cnt[second_pin][1] != 0){
+        // second Pin received signals
+        float quot = (1.0f * cnt[second_pin][1]) / cnt[first_pin][1];
+        if(quot > 0.25){
+          // second pin received significant amount of signal
+          float piece = 360.0f / (2.0f * pin_size);
+          int added_angle = piece * quot;
+          
+          if(second_pin == ((first_pin - 1) % pin_size)){
+            added_angle = -added_angle;  
+          }
+
+          noise_direction = noise_direction + added_angle;
+        }
+      }
+    }
+    for (int i=0; i<2; i++){
+        if(leftMics[i] == first_pin){
+            noise_direction = -noise_direction;
+            break;
+        }
+    }
+    noiseDetected(noise_direction);
+    Serial.println(noise_direction);
+  }
+  else if(!nd && new_noise){
+    //Noise Started
+    Serial.println("Noise started");
+    printValues(values);
+    nd = true;
+    for(int i = 0; i < pin_size; i++){
+      cnt[i][0] = pins[i];
+      cnt[i][1] = 0;
+    }
+    int max_pin = getMaxPin(values);
+    increaseMaxPinCount(max_pin);
+  }
+  else{
+    //nothing new
+  }
+}
+
+void printValues(int values[]){
+  for(int i = 0; i < pin_size; i++){
+    Serial.print(values[i]);
+    Serial.print(" ");
+  }
+  Serial.println("");
+}
+
+void increaseMaxPinCount(int maxPin){
+  for(int j = 0; j < pin_size; j++){
+    if(maxPin == cnt[j][0]){
+      cnt[j][1]++;
+    }
+  }
+}
+
+int getMaxPin(int values[]){
+  int max_out = 0;
+  int max_pin = 0;
+  for(int i = 0; i < pin_size; i++){
+    if(values[i] > max_out){
+      max_out = values[i];
+      max_pin = pins[i];
+    }
+  }
+  return max_pin;
+}
+
+void noiseDetected(float direction){
+    //micCounter = micCounter - 1;
+    if(currentState != Init){
+        Serial.println("noise detected executed");
+        Serial.println(enabledDrive);
+        killMovement();
+        setForwardDistance(40);
+        setTurnAngle((int) direction);
+        if(currentState == Idle){
+            setRobiState(Alerted);
+        }
+        else if(currentState == Alerted){
+            setRobiState(Hunting);
+        }
+    }
+    
+    
+}
+
+void killMovement(){
+    enabledDrive = false;
+    setDrivingState(DoNothing);
+}
+
 void setRobiState(RobiStates newState){
     if (currentState != newState){
+        //micCounter = 1;
         switch (newState)
             {
             case Init:
                 leds[0] = CRGB::Blue;
                 FastLED.show();
-                timerThreshold = 10000; //10sec
+                timerThreshold = 15000; //15sec
                 stateTimer.setTimeOutTime(timerThreshold);
                 stateTimer.reset();
+                //enabledMicArray = true;
                 callbackStateFunction = []() -> void {
                     attachInterrupt(digitalPinToInterrupt(pirPin), pirISR, CHANGE);
                     
@@ -169,22 +349,26 @@ void setRobiState(RobiStates newState){
                 timerThreshold = 10000; //alle 10sec bewegen
                 stateTimer.setTimeOutTime(timerThreshold);
                 stateTimer.reset();
+                //enabledMicArray = true;
 
-                testTimeInIdle = 0;
+                //testTimeInIdle = 0;
 
                 callbackStateFunction = []() -> void{
                     //move around random
                     
-                    if (testTimeInIdle>1){ //2
+                    setDrivingState(Random);
+                    
+                    /*if (testTimeInIdle>1){ //2
                         setRobiState(Alerted);
                     }else{
                         testTimeInIdle = testTimeInIdle + 1;
                         Serial.println("Exec Random");
                         setDrivingState(Random);
-                    }
+                    }*/
                     
                     
                 };
+                
                 break;
             case Alerted:
                 leds[0] = CRGB::Orange;
@@ -193,9 +377,13 @@ void setRobiState(RobiStates newState){
                 stateTimer.setTimeOutTime(timerThreshold);
                 stateTimer.reset();
                 timeInAlerted = 0;
-
+                //enabledMicArray = true;
+                //int arrSize = sizeof(melody)/sizeof(melody[0]);
+                //for (int thisNote = 0; thisNote < arrSize; thisNote++) { // Loop through the notes in the array.
+                //    TimerFreeTone(buzzerPin, melody[thisNote], duration[thisNote]); // Play thisNote for duration.
+                //}
                 //Test counter here
-                testCounterHunting = 0;
+                //testCounterHunting = 0;
 
                 callbackStateFunction = []() -> void{
                     // move around faster
@@ -204,10 +392,10 @@ void setRobiState(RobiStates newState){
                     if (timeInAlerted > alertedTimeThreshold){
                         setRobiState(Idle);
                     }
-                    if (testCounterHunting > 0){ //1
+                    /*if (testCounterHunting > 0){ //1
                         setRobiState(Hunting);
                     }
-                    testCounterHunting = testCounterHunting + 1;
+                    testCounterHunting = testCounterHunting + 1;*/
                 };
                 
                 break;
@@ -215,16 +403,18 @@ void setRobiState(RobiStates newState){
                 // catch & follow the sound
                 leds[0] = CRGB::Red;
                 FastLED.show();
-                int arrSize = sizeof(melody)/sizeof(melody[0]);
-                for (int thisNote = 0; thisNote < arrSize; thisNote++) { // Loop through the notes in the array.
-                    TimerFreeTone(buzzerPin, melody[thisNote], duration[thisNote]); // Play thisNote for duration.
-                }
-                //stateTimer.setTimeOutTime(1);
+
+                //enabledMicArray = true;
+                //int arrSize = sizeof(melody)/sizeof(melody[0]);
+                //for (int thisNote = 0; thisNote < arrSize; thisNote++) { // Loop through the notes in the array.
+                //    TimerFreeTone(buzzerPin, melody[thisNote], duration[thisNote]); // Play thisNote for duration.
+                //}
+                //stateTimer.setTimeOutTime(100000);
                 //stateTimer.reset();
                 //Get turn angle and distance from micro
                 //Set variables with setter methods (setTurnAngle, setForwardDistance)
-                setTurnAngle(90);
-                setForwardDistance(60);
+                //setTurnAngle(90);
+                //setForwardDistance(60);
                 setDrivingState(Turning);
                 //set variables, direction of sound, setDrivingState(Turning), setDrivingState(Forward)
                 callbackStateFunction = []() -> void{
@@ -250,11 +440,12 @@ void setDrivingState(DrivingStates newState){
                 timerDriveThreshold = 4000; //4sec fahren
                 driveTimer.setTimeOutTime(timerDriveThreshold);
                 driveTimer.reset();
-                
+                enabledDrive = true;
                 callbackDriveStateFunction = []() -> void {
+                    //enabledMicArray = false;
                     Application_FunctionSet.ApplicationFunctionSet_Obstacle();
                 };
-                enabledDrive = true;
+                
                 break;
             case Forward:
                 // MH: define meters (global variable) to drive forward
@@ -267,7 +458,9 @@ void setDrivingState(DrivingStates newState){
                 driveTimer.setTimeOutTime(timerDriveThreshold);
                 driveTimer.reset();
                 enabledDrive = true;
+                enabledMicArray = false;
                 callbackDriveStateFunction = []() -> void {
+                    //enabledMicArray = false;
                     Application_FunctionSet.ApplicationFunctionSet_DriveRobi(speed);
                 };
                 break;
@@ -276,20 +469,29 @@ void setDrivingState(DrivingStates newState){
                 // define turnAngle and get accordingly timerTurnTreshold
                 //turnAngle = 90; // future: will be defined by sensor
                 timerDriveThreshold = getTurnTimerThreshold(getTurnAngle());
+                Serial.print("Angle Threshold: ");
+                Serial.println(timerDriveThreshold);
                 driveTimer.setTimeOutTime(timerDriveThreshold);
                 driveTimer.reset();
                 enabledDrive = true;
+                enabledMicArray = false;
                 callbackDriveStateFunction = []() -> void{
-                   Application_FunctionSet.ApplicationFunctionSet_TurnRobi(TURN_LEFT, speed); 
+                    //enabledMicArray = false;
+                    int direction = TURN_RIGHT;
+                    if(getTurnAngle<0){
+                        direction = TURN_LEFT;
+                    }
+                   Application_FunctionSet.ApplicationFunctionSet_TurnRobi(direction, speed); 
                 };
                 
                 break;
             case DoNothing:
                 //
-                enabledDrive = false;
+                enabledDrive = false;            
                 //leds[0] = CRGB::Green;
                 //    FastLED.show();
                 Application_FunctionSet.ApplicationFunctionSet_StopRobi();
+                //enabledMicArray = true;
                 //callbackDriveStateFunction = []() -> void{
                 //    Application_FunctionSet.ApplicationFunctionSet_StopRobi();
                 //};
